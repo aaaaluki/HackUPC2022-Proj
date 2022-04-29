ecc69778ca2ba3a8acc5a8da42b73c0ece39d8a6
aaaaluki <luki00.lrb@gmail.com>
2022-04-30 01:00:18 +0200

    Config file

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+DEBUG = True
+
+DB_HOST = 'localhost'
+DB_NAME = 'moto'
+DB_USER = 'postgres'
+DB_PASS = None
+
+ID = 0
+NAME = 1
+BRAND_ID = 2
+YEAR = 3
+FUEL = 4
+PRICE = 5
+
--- main.py
+++ main.py
@@ -2,20 +2,8 @@
 
 import psycopg2
 
+from config import *
 
-DEBUG = True
-
-DB_HOST = 'localhost'
-DB_NAME = 'moto'
-DB_USER = 'postgres'
-DB_PASS = None
-
-ID = 0
-NAME = 1
-BRAND_ID = 2
-YEAR = 3
-FUEL = 4
-PRICE = 5
 
 QUERY_ID = 100
 
