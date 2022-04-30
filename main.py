5ae859c9e03cffc53142b50cb2bde0698b21365e
Javier <jafear11@gmail.com>
2022-04-30 18:40:27 +0200

    Hola

--- main.py
+++ main.py
@@ -18,14 +18,19 @@ class Window(tk.Tk):
     def __init__(self, matches, moto):
         super().__init__()
         self.title('Comparar motocicleta')
-        figure_canvas = FigureCanvasTkAgg(grafiques.graficar(matches, moto), self)
+        figure_canvas = FigureCanvasTkAgg(
+            grafiques.graficar(matches, moto), self)
         NavigationToolbar2Tk(figure_canvas, self)
 
 
 def main():
     wrapper = DBWrapper(DB_HOST, DB_NAME, DB_USER, DB_PASS, DB_PORT)
-
-    query = wrapper.get_moto(123)
+    id_request = tk.Tk()
+    entrada = tk.Entry(id_request)
+    enter = tk.Button(id_request, text="Enter",
+                      command=wrapper.get_moto(123))
+    query=wrapper.get_moto(123)
+    id_request.mainloop()
 
     filters = []
     filters.append(DBFilter(PRICE, dbfilter.LT, 6000))
