66901ecece3444379a847b49a3467e558d78ca52
aaaaluki <luki00.lrb@gmail.com>
2022-04-30 16:21:56 +0200

    merged test_dbwrapper and main

--- test_dbwrapper.py
+++ test_dbwrapper.py
@@ -5,7 +5,7 @@ from dbwrapper import DBWrapper
 from dbfilter import DBFilter
 
 
-def Test():
+def test():
     wrapper = DBWrapper(DB_HOST, DB_NAME, DB_USER, DB_PASS, DB_PORT)
     # query = wrapper.get_moto(100)
     # query = wrapper.filter_by_param(NAME, 'R 100 RT   (1978-1996)')
@@ -13,12 +13,10 @@ def Test():
     filters.append(DBFilter(PRICE, dbfilter.LT, 6000))
     filters.append(DBFilter(PRICE, dbfilter.GE, 5990))
     query = wrapper.apply_filters(filters)
-    
-    #print(query)
-    #print("+++++++++++++++++++++++++++query returned by Test_DB_Wrapper")
+
     return query
 
 
 if __name__ == "__main__":
-    Test()
+    test()
 
