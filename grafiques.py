e187f13218d9f2e3f72b52c728c3cd3c349a5fb0
aaaaluki <luki00.lrb@gmail.com>
2022-04-30 19:14:50 +0200

    Fixing broken stuff and comments

--- grafiques.py
+++ grafiques.py
@@ -3,17 +3,19 @@ import matplotlib.pyplot as plt
 from config import *
 
 
+# Reference moto preferences
 MOTO_COLOR = 'red'
 MOTO_LW = 6
 
 
-# Funcio que grafica les variables
-# ---*matrix es la matriu amb totes les motos on cada columna es
-# ---una variable
-# ---preu_moto es el preu de la moto que correspon al id que hem
-# ---*seleccionat previament
-
 def graficar(matrix, moto):
+    """
+    Plots all the motos in matrix and the reference one
+
+    :param matrix: Nx6 matrix for N motos to show
+    :param moto: reference moto
+    :return: fig
+    """
     prices = matrix[:,PRICE]
     name = matrix[:,NAME]
     year = matrix[:,YEAR]
@@ -22,30 +24,34 @@ def graficar(matrix, moto):
 
     fig, axs = plt.subplots(2, 2, constrained_layout=True)
 
+    # Model ID plot
     axs[0, 0].scatter(name, prices)
     axs[0, 0].scatter(moto[NAME], moto[PRICE], c=MOTO_COLOR, linewidth=MOTO_LW)
-    axs[0, 0].set_title('Model == name')
-    axs[0, 0].set_ylabel('Preu (€)')        
+    axs[0, 0].set_title('Model ID')
+    axs[0, 0].set_ylabel('Price €')
     axs[0, 0].get_xaxis().set_visible(False)
 
+    # Year plot
     axs[0, 1].scatter(year, prices)
     axs[0, 1].scatter(moto[YEAR], moto[PRICE], c=MOTO_COLOR, linewidth=MOTO_LW)
-    axs[0, 1].set_title('any')
-    axs[0, 1].set_ylabel('Preu (€)')
+    axs[0, 1].set_title('Year')
+    axs[0, 1].set_ylabel('Price €')
     axs[0, 1].get_xaxis().set_visible(True)
 
+    # Brand ID plot
     axs[1, 0].scatter(brand, prices)
     axs[1, 0].scatter(moto[BRAND_ID], moto[PRICE], c=MOTO_COLOR, linewidth=MOTO_LW)
-    axs[1, 0].set_title('marca == brand')
-    axs[1, 0].set_ylabel('Preu (€)')
+    axs[1, 0].set_title('Brand ID')
+    axs[1, 0].set_ylabel('Price €')
     axs[1, 0].get_xaxis().set_visible(False)
 
+    # Fuel plot
     axs[1, 1].scatter(fuel, prices)
     axs[1, 1].scatter(moto[FUEL], moto[PRICE], c=MOTO_COLOR, linewidth=MOTO_LW)
-    axs[1, 1].set_title('gasolina')
-    axs[1, 1].set_ylabel('Preu (€)')
+    axs[1, 1].set_title('Fuel')
+    axs[1, 1].set_ylabel('Price €')
     axs[1, 1].get_xaxis().set_visible(True)
 
-    fig.suptitle('Comparació multiparamètrica ', fontsize=20)
+    fig.suptitle('Multiparams comparison', fontsize=20)
     plt.show()
     return fig
--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python3
-import tkinter as tk
-import matplotlib
 
-import dbfilter
-import grafiques
-from config import *
-from dbwrapper import DBWrapper
-from dbfilter import DBFilter
+import tkinter as tk
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg,
     NavigationToolbar2Tk
 )
+
 import grafiques
-from Test_DB_Wrapper import *
-#import dbwrapper
-#import dbfilter
+import dbfilter
+from config import *
+from dbwrapper import DBWrapper
+from dbfilter import DBFilter
 
 
 class Window(tk.Tk):
     def __init__(self, matches, moto):
         super().__init__()
-        self.title('Comparar motocicleta')
+        self.title('Compare motomami')
         figure_canvas = FigureCanvasTkAgg(grafiques.graficar(matches, moto), self)
         NavigationToolbar2Tk(figure_canvas, self)
 
@@ -28,6 +24,7 @@ class Window(tk.Tk):
 def main():
     wrapper = DBWrapper(DB_HOST, DB_NAME, DB_USER, DB_PASS, DB_PORT)
 
+    # Some sample data
     query = wrapper.get_moto(123)
 
     filters = []
