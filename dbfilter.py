ae3ef83559640d8128f4a0d7368afd65fa7e73ec
aaaaluki <luki00.lrb@gmail.com>
2022-04-30 12:14:11 +0200

    Added a DBFilter class

---
+++ dbfilter.py
@@ -0,0 +1,34 @@
+
+# Bash (test) inspired names
+EQ = 0  # Equal          ==
+NE = 1  # Not equal      !=
+LT = 2  # Less than      <
+GT = 3  # Greater than   >
+GE = 4  # Greater equal  >=
+LE = 5  # Less equal     <=
+
+CMP_MAPPING = {EQ: lambda a, b: a == b,
+               NE: lambda a, b: a != b,
+               LT: lambda a, b: a < b,
+               GT: lambda a, b: a > b,
+               GE: lambda a, b: a >= b,
+               LE: lambda a, b: a <= b}
+
+
+class DBFilter:
+
+    def __init__(self, param, cmp, param_value):
+        """
+
+        :param param: Column of the table
+        :param cmp: Type of comparison
+        :param param_value: Value to compare
+        """
+
+        self.param = param
+        self.cmp = cmp
+        self.param_value = param_value
+
+    def apply(self, db):
+        # THE HORROR
+        return db[CMP_MAPPING[self.cmp](db[self.param], self.param_value)]
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-
+import dbfilter
 from config import *
 from dbwrapper import DBWrapper
+from dbfilter import DBFilter
 
 
 def main():
     wrapper = DBWrapper(DB_HOST, DB_NAME, DB_USER, DB_PASS, DB_PORT)
     # query = wrapper.get_moto(100)
-    query = wrapper.filter_by_param(NAME, 'R 100 RT   (1978-1996)')
+    # query = wrapper.filter_by_param(NAME, 'R 100 RT   (1978-1996)')
+    fltr = DBFilter(NAME, dbfilter.EQ, 'R 100 RT   (1978-1996)')
+    query = fltr.apply(wrapper.df_versions).values
 
     print(query)
 
